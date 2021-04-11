310e3d223ca5fad5d91d410e967ec1374ef5edb0
toytoy08 <toy31327@gmail.com>
2021-04-12 04:49:15 +0700

    start backend to connect db

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello first path backend')
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
