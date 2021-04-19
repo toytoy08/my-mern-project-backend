d5a106475e3ba904b19887371c667e622bb9251b
toytoy08 <toy31327@gmail.com>
2021-04-20 03:31:08 +0700

    Update : Add update with id by params on Patch method

--- controller/posts.js
+++ controller/posts.js
@@ -25,11 +25,22 @@ export const createPost = async (reg, res) => {
 export const deletePost = async (reg, res) => {
     const { id } = reg.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id to delete');
 
     await PostMessage.findByIdAndRemove(id);
 
     console.log('DELETE SUCCESSFULLY!!');
 
     res.json({ message: 'Post deleted successfully' });
+}
+
+export const updatePost = async (reg, res) => {
+    const { id: _id } = reg.params;
+    const post = reg.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id to update');
+
+    const updatePost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+
+    res.json(updatePost);
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getPosts, createPost, deletePost } from '../controller/posts.js';
+import { getPosts, createPost, deletePost, updatePost } from '../controller/posts.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
 router.delete('/:id', deletePost);
+router.patch('/:id', updatePost);
 
 export default router;
